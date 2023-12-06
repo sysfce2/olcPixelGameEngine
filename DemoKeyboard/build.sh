40599406e015c2582974cd840d567ae57238fb8b
Moros1138 <moros1138@gmail.com>
2023-12-05 21:17:41 -0500

    add instructions to the build script.

--- DemoKeyboard/build.sh
+++ DemoKeyboard/build.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/bash
 
-clang++ -I.. -o DemoKeyboard-glut DemoKeyboard.cpp -O2 -lglut -lGL -lm -lpng -lstdc++fs -std=c++20 -DOLC_PLATFORM_GLUT
+#
+# Find and uncomment your platform and comment out the rest!
+#
 
+# MacOS GLUT
+# clang++ -I.. -o DemoKeyboard-mac DemoKeyboard.cpp -arch x86_64 -std=c++20 -mmacosx-version-min=10.15 -Wall -framework OpenGL -framework GLUT -framework Carbon -lpng
+
+# Linux XLIB
 clang++ -I.. -o DemoKeyboard-xlib DemoKeyboard.cpp -O2 -lX11 -lGL -lm -lpthread -lpng -lstdc++fs -std=c++20
 
-em++ -I.. -o DemoKeyboard.html DemoKeyboard.cpp -O2 -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=2 -s USE_LIBPNG=1 -std=c++20 --preload-file assets@assets
+# LInux GLUT
+# clang++ -I.. -o DemoKeyboard-glut DemoKeyboard.cpp -O2 -lglut -lGL -lm -lpng -lstdc++fs -std=c++20 -DOLC_PLATFORM_GLUT
 
-x86_64-w64-mingw32-g++ -I.. -o DemoKeyboard.exe DemoKeyboard.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lshlwapi -ldwmapi -lstdc++fs -std=c++20 -static
+# Emscripten
+# em++ -I.. -o DemoKeyboard.html DemoKeyboard.cpp -O2 -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=2 -s USE_LIBPNG=1 -std=c++20 --preload-file assets@assets
+
+# Windows via MinGW
+# x86_64-w64-mingw32-g++ -I.. -o DemoKeyboard.exe DemoKeyboard.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lshlwapi -ldwmapi -lstdc++fs -std=c++20 -static
+
